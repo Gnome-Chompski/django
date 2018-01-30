716f4de38ac1dabea20b1af24dbaec3479d08cc6
Dante <kIjygg38@gmail.com>
2018-01-30 19:14:00 +0000

    Chapter 8 complete

--- rango/models.py
+++ rango/models.py
@@ -6,6 +6,7 @@ class Category(models.Model):
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
+    models.max_length = 128
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
